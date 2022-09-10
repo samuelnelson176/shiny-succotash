96d3899145fa3c206732de1eae323dc6342586b1
samuelnelson176 <106776244+samuelnelson176@users.noreply.github.com>
2022-09-10 10:04:31 -0700

    Add files via upload

---
+++ index.js
@@ -0,0 +1,18 @@
+let randomNumber = Math.floor(Math.random() * 100) +1;
+
+const guesses = document.querySelector('.guesses');
+
+const lastResult =  document.querySelector('.lastResult');
+
+const lowOrHi =  document.querySelector('.lowOrHi');
+
+const guessSubmit =  document.querySelector('.guessSubmit');
+
+const guessField =  document.querySelector('.guessField');
+
+let guesssCount = 1;
+let resetButton;
+
+function checkGuess() {
+    alert('I Am a Placeholder');
+};
